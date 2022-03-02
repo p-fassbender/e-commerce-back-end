dfde705c00cf386f19990f839fda8ac6d6f67db0
Preston Fassbender <fassbenderp0551@gmail.com>
2022-03-02 17:31:44 -0600

    added model column definitions

--- models/Tag.js
+++ models/Tag.js
@@ -2,19 +2,28 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
-  }
+    {
+        // define columns - id, tag_name
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        tag_name: {
+            type: DataTypes.STRING
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'tag',
+    }
 );
 
 module.exports = Tag;
