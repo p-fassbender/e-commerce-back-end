77a491c140313557f5fb0d854362c5dff88f5c9e
Preston Fassbender <fassbenderp0551@gmail.com>
2022-03-02 17:56:21 -0600

    model associations

--- models/index.js
+++ models/index.js
@@ -5,16 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    as: 'tagged_products',
+    foreignKey: 'product_id',
+    onDelete: 'SET NULL'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    as: 'tagged_products',
+    foreignKey: 'tag_id',
+    onDelete: 'SET NULL'
+});
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
